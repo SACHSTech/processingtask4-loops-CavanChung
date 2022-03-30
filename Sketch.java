50fb12765fde34abb05a0c118054d7de1321c30b
Cavan Chung <75390863+CavanChung@users.noreply.github.com>
2022-03-30 04:34:55 +0000

    Sorry, I ended up misreading your classroom message about moving the livehack and thought the processing task due today was moved to Friday until I doubled checked due date a few hours before it was due.

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,51 @@
+/**
+ * Name:Processing Task 4
+ * Purpose:Loop quadrants
+ * Author: Cavan Chung
+ * Created:2022/03/30
+ */
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
   
-  // define other methods down here.
-}
+  public void setup() {
+    background(255);
+  }
+
+  public void draw() {
+    //Quad 1 - Draws ten by ten grid by drawing 10 x-lines and 10 y-lines, stopping at width/2 or half the window
+    for (int lineXY = 0; lineXY <= width / 2; lineXY += (width / 20)) {
+      stroke(0);
+      line(lineXY, 0, lineXY, height / 2);
+      line(0, lineXY, width / 2, lineXY);
+    }
+    //Quad 2 - Nested loop, draws a 5 rows of 5 circles
+    for (int circleY = height / 2 / 6; circleY <= height / 2 - (height / 2 / 6); circleY += height / 2 / 6) {
+      for (int circleX = (width / 2) + width / 2 / 6; circleX <= width - (width / 2 / 6); circleX += width / 2 / 6) {
+        fill(231, 84, 128);
+        ellipse(circleX, circleY, 20, 20);
+      }
+    }
+    //Quad 3 - line colour and location is added by plus one every loop and stops at half the window size
+    for (int lineX = 0; lineX <= width/2; lineX++) {
+        line(lineX, height/2, lineX, height);  
+        stroke(lineX);
+    }
+    //Quad 4 - Draw and rotate flower, while placing it in quadrant 4 and using f to convert to float
+
+    stroke(0);
+    translate(width / 2.1f + width / 4.1f, height / 2.1f + height / 4.1f);
+    
+    for (int i = 0; i <= 8; i += 1) {
+      rotate(radians(45));
+      fill(255, 255, 224);
+      ellipse(0, 0, width/20, height/3);  
+    }
+    fill(0, 0, 0);
+    ellipse(0, 0, width/6, height/6);
+    }
+  }
